632460116eefd4cb9a11a0ebc096c7fe93526e1c
jd-keith <42483240+jd-keith@users.noreply.github.com>
2020-03-23 16:29:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Hapi = require('hapi');
 const Hoek = require('hoek');
 const azure = require('azure-storage');
 const storage = require('./utility/storage');
-const tableService = azure.createTableService(process.env.AZURE_STORAGE_ACCOUNT, process.env.AZURE_STORAGE_ACCOUNT_KEY);
+const tableService = azure.createTableService(process.env.AZURE_STORAGE_CONNECTION_STRING);
 
 const server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000, host: 'localhost' })
