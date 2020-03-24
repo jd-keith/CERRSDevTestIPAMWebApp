fbf7a12a3361bab1a2d319dcd2a9c8d8e00fdae6
jd-keith <42483240+jd-keith@users.noreply.github.com>
2020-03-24 16:25:49 -0400

    Update storage.js

--- utility/storage.js
+++ utility/storage.js
@@ -53,6 +53,6 @@ module.exports.getLastNRows = function(azure, tableService, columns, n, sort, ca
     //const sortStrategy = (sort === 'Timestamp') ? byTime : byField(sort);
     //const sorted = rows.slice().sort(sortStrategy);
 
-    return callback(null, sorted);
+    return callback(null);
   });
 };
