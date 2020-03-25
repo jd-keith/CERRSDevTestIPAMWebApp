fd1ae7264dc07870957aa8fa921487a0c81286be
jd-keith <42483240+jd-keith@users.noreply.github.com>
2020-03-25 13:21:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,67 +13,67 @@ const server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000, host: 'localhost' })
 
 server.register(require('inert'), (err) => {
-  server.route({
-    method: 'GET',
-    path: '/css/{param*}',
-    handler: {
-        directory: {
-            path: 'public/css'
+    server.route({
+        method: 'GET',
+        path: '/css/{param*}',
+        handler: {
+            directory: {
+                path: 'public/css'
+            }
         }
-    }
-  });
+    });
 
-  server.route({
-    method: 'GET',
-    path: '/fonts/{param*}',
-    handler: {
-        directory: {
-            path: 'public/fonts'
+    server.route({
+        method: 'GET',
+        path: '/fonts/{param*}',
+        handler: {
+            directory: {
+                path: 'public/fonts'
+            }
         }
-    }
-  });
+    });
 });
 
 server.register(require('vision'), (err) => {
 
-  Hoek.assert(!err, err);
+    Hoek.assert(!err, err);
 
-  server.views({
-    engines: {
-        html: require('handlebars')
-    },
-    relativeTo: __dirname,
-      path: './templates',
-    layoutPath: './templates/layout'
-  });
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: './templates',
+        layoutPath: './templates/layout'
+    });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-      const numRows = request.query.rows ? request.query.rows : 100;
-      const columns = columnNames;
-      //const sort = request.query.sort && columns.includes(request.query.sort) ? request.query.sort : 'Timestamp';
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            const numRows = request.query.rows ? request.query.rows : 100;
+            const columns = columnNames;
+            //const sort = request.query.sort && columns.includes(request.query.sort) ? request.query.sort : 'Timestamp';
 
-      //storage.getLastNRows(azure, tableService, columns, numRows, function(error, rows) {
-        //if (error) {
-          //console.log(error);
-          //return reply(error);
-        //}
-        
-        const viewData = {
-          rows: rows,
-          storageName: 'cerrsdevtestipam',
-          tableName: 'AzureIPAMTable'
-        };
+            //storage.getLastNRows(azure, tableService, columns, numRows, function(error, rows) {
+            //if (error) {
+            //console.log(error);
+            //return reply(error);
+           // }
 
-        reply.view('index', viewData, { layout: 'main'});
-      });
-    }
+            const viewData = {
+                rows: rows,
+                storageName: 'cerrsdevtestipam',
+                tableName: 'AzureIPAMTable'
+            };
+
+            reply.view('index', viewData, { layout: 'main' });
+        //});
+}
   });
 });
 
 server.start((err) => {
-  if (err) { throw err; }
-  console.log(`Server running at: ${server.info.uri}`);
+    if (err) { throw err; }
+    console.log(`Server running at: ${server.info.uri}`);
 });
